b21073177c296b1ef85fb683bf86f05d1ead1229
CeubNicolasKlaczko <nicolas.klaczko@sempreceub.com>
2025-02-26 08:40:30 -0300

    primeiro exersisio aula 2 python

---
+++ soma_de_variavel.py
@@ -0,0 +1,16 @@
+valor1 = float(input("valor 1 "))
+valor2 = float(input("valor 2 "))
+oprtasao = str(input("qual operasao deseja faser "))
+if oprtasao == "+":
+
+    resultado = (valor1 + valor2)
+    print (resultado)
+elif oprtasao == "-":
+    resultado = (valor1 - valor2)
+    print (resultado)
+elif oprtasao == "*":
+    resultado = (valor1 * valor2)
+    print (resultado)
+elif oprtasao == "/":
+    resultado = (valor1 / valor2)
+    print (resultado)
