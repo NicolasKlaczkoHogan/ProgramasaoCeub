2820c8e83597661633ee01ab330527733644e742
CeubNicolasKlaczko <nicolas.klaczko@sempreceub.com>
2025-04-02 13:04:26 -0300

    java_script aula 1

---
+++ java_scripet/ingreso.js
@@ -0,0 +1,12 @@
+let idade = 18
+let ingreso = true
+if (idade>17){
+    if (ingreso === true){
+        console.log("pode entrar")
+    }else{
+        console.log("nao tem ingreso")
+    }
+    
+}else{
+    console.log("nao é velo o bastante")
+}
---
+++ java_scripet/teste.js
@@ -0,0 +1,15 @@
+let nome = "Nicolas"
+let idade = 17
+let estudante = true
+console.log(`Nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`estudante: ${estudante}`)
+
+let saldo = 500 
+if (saldo > 0){
+    console.log("conta positiva")
+} else if (saldo < 0){
+    console.log("conta negativa")
+}else {
+    console.log("saldo = 0")
+}
